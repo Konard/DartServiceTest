d022fb382dfd2ebbe3e04950bc7d940bb0dc2205
Binatik <sadxas999@mail.ru>
2020-04-26 15:35:54 +0300

    js code not  Source

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -6,13 +6,36 @@ burger.addEventListener("click", function () {
     navigation.classList.toggle('active');
 });
 
+//Эта версия работает, но при наведении добавляет класс всем пунктам. 
 const rowLink = document.querySelectorAll('.row__link');
 const rowSvg = document.querySelectorAll('.row__svg');
-rowLink.forEach(function (item) {
-    item.addEventListener("mouseover", function () {
-        rowSvg.forEach(function (item) {
-            item.classList.toggle('induced');
-            console.dir(rowSvg);
-        });
+
+rowLink.forEach(function (element) {
+    element.addEventListener("mouseover", function () {
+        for (let i = 0; i < rowSvg.length; i++) {
+            rowSvg[i].classList.add("induced");
+        }
     });
-});
+    element.addEventListener("mouseout", function () {
+        for (let i = 0; i < rowSvg.length; i++) {
+            rowSvg[i].classList.remove("induced");
+        }
+    });
+})  
+
+
+
+/* Эта версия не работает.
+const rowLink = document.querySelectorAll('.row__link');
+const rowSvg = document.querySelector('.row__svg');
+rowLink.forEach(function (element) {
+    element.addEventListener("mouseover", function () {
+        rowSvg.classList.add("induced"); 
+        console.dir(rowSvg);
+    });
+    element.addEventListener("mouseout", function () {
+        rowSvg.classList.remove("induced"); 
+        console.dir(rowSvg);
+    });
+}) 
+*/
